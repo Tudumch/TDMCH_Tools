a485b941a09280cad72270269cfb0e78bc8d034d
tudumch <567-45@mail.ru>
2024-05-17 12:09:59 +0300

    Rewrite cleanup script to make it more universal

---
+++ DevOps/CleanAndRegenerate/setup.bat
@@ -0,0 +1,24 @@
+@echo off
+
+:: Puts Cleanup script files into UE-project root directory.
+
+:: Changing path to project root dir
+cd ".."
+cd ".."
+cd ".."
+cd ".."
+
+:: Creating script folder structure and copying initial files
+set PROJECT_DIR=%cd%
+set PLUGIN_FOLDERS_STRUCTURE=DevOps\CleanAndRegenerate
+set FULL_SCRIPT_PATH=%PROJECT_DIR%\%PLUGIN_FOLDERS_STRUCTURE%
+
+mkdir %FULL_SCRIPT_PATH%
+
+xcopy /s %~dp0\ScriptFiles\** %FULL_SCRIPT_PATH%
+mklink  "%PROJECT_DIR%\CleanAndRegenerate.bat" "%FULL_SCRIPT_PATH%\CleanAndRegenerate.bat"
+
+echo.
+echo CleanUp script setup was successfull. You can make additional tweaks for it into "YOUR_PROJECT_FOLDER/DevOps/CleanAndRegenerate/config.bat"
+echo.
+pause
